8c1ae5a051318ba3d2284e9797bc4dede8b40f2b
Judit Talas <judit.talas@jic.ac.uk>
2024-07-10 17:00:22 +0100

    Defined config.sh file
    Contains inputs for Project ID, reads directory, log directory and outputs directory

---
+++ bash-1/config/config.sh
@@ -0,0 +1,11 @@
+# Project ID
+PROJECT_ID="83"
+
+# Directory containing the paired-end read files
+READS_DIR="001.csv_question"
+
+# Output directory
+OUTPUT_DIR="output"
+
+# Log directory
+LOG_DIR="logs"
