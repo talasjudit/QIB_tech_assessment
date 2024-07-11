f43bfe6a9c4aac9abe4fdc45d7e72399b9a2fba6
Judit Talas <judit.talas@jic.ac.uk>
2024-07-11 12:30:29 +0100

    Q1 Changes to config file and blast query script
    Changed name of config time to be specific for first question as log dir is specific to Q1
    First iteration of blast query script with remote search

--- genome/scripts/01_species_identification.sh
+++ genome/scripts/01_species_identification.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
 # Load the configuration variables
-source config/config.sh
+source config/01_config.sh
 
+# Create a log file
+LOG_FILE="$LOG_DIR/blast_log.txt"
+
+# Run BLAST to identify the species using NCBI remote database within Singularity
+singularity exec $SINGULARITY_FILE blastn \
+    -query $GENOME_FASTA \
+    -db $BLAST_DB \
+    -remote \
+    -out $OUTPUT_DIR/blast_results.txt \
+    -outfmt '6 std staxids' \
+    -max_target_seqs 1 \
+    &> $LOG_FILE
+
+echo "Species identification results are saved in $LOG_DIR/blast_results.txt"
+echo "Log file is saved in $LOG_FILE"
